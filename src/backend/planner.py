423c565f78a5074d8b4297f3823b4a5be3839d95
Wei Lun <weilunn.97@gmail.com>
2020-09-11 00:02:30 +0800

    Added src folder

---
+++ src/backend/planner.py
@@ -0,0 +1,9 @@
+"""
+MAIN TIMETABLING LOGIC / SEARCH ALGORITHMS TO GENERATE
+TIMETABLE COMBINATIONS GO HERE
+"""
+
+
+class Planner:
+    def __init__(self):
+        pass
---
+++ src/backend/time_parser.py
@@ -0,0 +1,5 @@
+class TimeParser:
+
+    @staticmethod
+    def get_timeslot(t_start, t_end) -> int:
+        pass
