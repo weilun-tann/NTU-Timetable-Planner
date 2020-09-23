59e93962e360c66f4b98dc82202756ef4767853f
Sunny Pek <sunny1996@live.com.sg>
2020-09-23 10:10:59 +0800

    front end data demo

--- src/application/planner/views.py
+++ src/application/planner/views.py
@@ -23,4 +23,14 @@ def timetable(request: HttpRequest) -> HttpResponse:
     :param request: To view the timetable page
     :return: The timetable page
     """
-    return render(request, 'timetable.html', {})
+    context = {"d": {"s1":{"name":"FASHION & DESIGN: WEARABLE ART AS A SECOND SKIN*",
+                    "au":" 3.0 AU",
+                    "index":"aaaaaaaaaaa"},
+               "s2": {"name": "ART",
+                      "au": " 3.0 AU",
+                      "index": "aaaaaaaaaaa"},
+               "s3": {"name": "WEARABLE ART AS A SECOND SKIN*",
+                      "au": " 3.0 AU",
+                      "index": "aaaaaaaaaaa"}
+               }}
+    return render(request, 'timetable.html', context)
