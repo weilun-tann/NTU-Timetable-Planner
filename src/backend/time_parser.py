2769692f2ae2ca955f8eadb5d7672eb494f7fdbd
Wei Lun <weilunn.97@gmail.com>
2020-09-23 13:49:49 +0800

    Added backend timetable combination logic

--- src/backend/time_parser.py
+++ src/backend/time_parser.py
@@ -1,10 +1,13 @@
-from typing import List, Set
-
-from .json_parser import Timeslot
+from typing import Set
 
 
 class TimeParser:
 
     @staticmethod
     def get_timeslots(t_start, t_end) -> Set[int]:
+        # times = ["0830", "0930", "1030", "1130", "1230",
+        #          "1330", "1430", "1530", "1630", "1730",
+        #          "1830", "1930", "2030", "2130", "2230"]
+        # time_to_slot = {times[i]: i for i in range(len(times) - 1)}
+        # return time_to_slot.get(t_start)
         pass
