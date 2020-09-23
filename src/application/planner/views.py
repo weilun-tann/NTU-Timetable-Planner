01bc30de5c776aa72ac2a85df28af95c2aee451f
Wei Lun <weilunn.97@gmail.com>
2020-09-23 13:50:59 +0800

    Update views.py

--- src/application/planner/views.py
+++ src/application/planner/views.py
@@ -25,21 +25,18 @@ def timetable(request: HttpRequest) -> HttpResponse:
     :param request: To view the timetable page
     :return: The timetable page
     """
-<<<<<<< Updated upstream
-    context = {"d": {"s1":{"name":"FASHION & DESIGN: WEARABLE ART AS A SECOND SKIN*",
-                    "au":" 3.0 AU",
-                    "index":"aaaaaaaaaaa"},
-               "s2": {"name": "ART",
-                      "au": " 3.0 AU",
-                      "index": "aaaaaaaaaaa"},
-               "s3": {"name": "WEARABLE ART AS A SECOND SKIN*",
-                      "au": " 3.0 AU",
-                      "index": "aaaaaaaaaaa"}
-               }}
+    context = {"d": {"s1": {"name": "FASHION & DESIGN: WEARABLE ART AS A SECOND SKIN*",
+                            "au": " 3.0 AU",
+                            "index": "aaaaaaaaaaa"},
+                     "s2": {"name": "ART",
+                            "au": " 3.0 AU",
+                            "index": "aaaaaaaaaaa"},
+                     "s3": {"name": "WEARABLE ART AS A SECOND SKIN*",
+                            "au": " 3.0 AU",
+                            "index": "aaaaaaaaaaa"}
+                     }}
     return render(request, 'timetable.html', context)
-=======
 
     # @SUNNY, CHANGE THIS LINE TO GENERATE COMBIS FOR DIFFERENT COURSES YOU WANNA PLAN FOR
     data = {"combinations": Planner.generate_combis(["CZ2001", "CZ2002", "CZ2003", "CZ2004", "CZ2005", "CZ2006"])}
     return render(request, 'timetable.html', data)
->>>>>>> Stashed changes
