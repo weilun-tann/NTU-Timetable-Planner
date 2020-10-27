a1fa77d407678a4a67c2ed3c85498e926f74ce4d
Wei Lun <weilunn.97@gmail.com>
2020-10-27 20:20:17 +0800

    Update tests.py

--- src/backend/tests.py
+++ src/backend/tests.py
@@ -14,18 +14,18 @@ class BackendTests(unittest.TestCase):
         EC2 (VALID) : L >= FIXED
         EC1 (INVALID) : L CROSSES FIXED IN FRONT
         EC2 (INVALID) : L CROSSES FIXED AT THE BACK
+
+        FINALLY, PERFORM BVT ON VALID EC
         """
-        # EC1 (VALID)
+        # EC1 (VALID) - LOWER BOUNDARY OF 1030
+        self.assertEqual(False, Planner.clashes([("MON", "09:00:00", "10:00:00"), ("MON", "10:30:00", "11:30:00")]))
         self.assertEqual(False, Planner.clashes([("MON", "09:30:00", "10:30:00"), ("MON", "10:30:00", "11:30:00")]))
-
-        # EC2 (VALID)
-        self.assertEqual(False, Planner.clashes([("MON", "11:30:00", "12:30:00"), ("MON", "10:30:00", "11:30:00")]))
-
-        # EC3 (INVALID)
         self.assertEqual(True, Planner.clashes([("MON", "10:00:00", "11:00:00"), ("MON", "10:30:00", "11:30:00")]))
 
-        # EC4 (INVALID)
+        # EC2 (VALID) - UPPER BOUNDARY OF 1130
         self.assertEqual(True, Planner.clashes([("MON", "11:00:00", "12:00:00"), ("MON", "10:30:00", "11:30:00")]))
+        self.assertEqual(False, Planner.clashes([("MON", "11:30:00", "12:30:00"), ("MON", "10:30:00", "11:30:00")]))
+        self.assertEqual(False, Planner.clashes([("MON", "12:00:00", "13:00:00"), ("MON", "10:30:00", "11:30:00")]))
 
 
 if __name__ == "__main__":
